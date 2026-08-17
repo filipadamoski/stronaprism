d637e454a50fb780b6f2dd59999e318ec511c591
Filip Adamoski <filip@example.com>
2026-08-17 12:06:10 +0200

    feat: initialize Next.js project

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+export const metadata = {
+  title: 'Pogoda',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  )
+}
